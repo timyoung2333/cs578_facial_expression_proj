e70c042ed46a6bbff465b48e67d74f9814102eeb
Tao Li <root@tao.li>
2020-10-25 16:42:46 -0400

    update dataset wrapper (change API)

--- code/FER2013.py
+++ code/FER2013.py
@@ -9,8 +9,8 @@ class FER2013:
         """
         Load data
         """
-        self.X = {}
-        self.Y = {}
+        self.X_dic = {}
+        self.Y_dic = {}
 
         with open(filename) as f:
 
@@ -22,8 +22,8 @@ class FER2013:
                 emotion, usage, pixels = line.split(",")
                 pixels = np.asarray([int(j) for j in pixels.split(" ")])
 
-                self.X[i] = self.encoding(pixels)
-                self.Y[i] = emotion
+                self.X_dic[i] = self.encoding(pixels)
+                self.Y_dic[i] = emotion
 
                 i += 1
 
@@ -34,14 +34,48 @@ class FER2013:
         """
         return vec
 
-    def getX(self, img_id):
+    def getVector(self, img_id):
         """
         Returen feature vector
         """
-        return self.X[img_id]
+        return self.X_dic[img_id]
 
-    def getY(self, img_id):
+    def getLabel(self, img_id):
         """
         Return image label
         """
-        return self.Y[img_id]
+        return self.Y_dic[img_id]
+
+    def getTrainset(self, id_list):
+        """
+        Input: the IDs of images in the train set, id_list
+        Output: matrix X of features, with n rows (samples), d columns (features)
+                    X(i,j) is the j-th feature of the i-th sample
+                vector y of labels, with n rows (samples), 1 column
+                    y(i) is the label (+1 or -1) of the i-th sample
+        """
+        X = []
+        y = []
+
+        for i in id_list:
+            X.append(self.getVector(i))
+            y.append(self.getLabel(i))
+
+        return X, y
+
+    def getTestset(self, id_list):
+        """
+        Input: the IDs of images in the test set, id_list
+        Output: matrix X of features, with n rows (samples), d columns (features)
+                    X(i,j) is the j-th feature of the i-th sample
+                vector y of labels, with n rows (samples), 1 column
+                    y(i) is the label (+1 or -1) of the i-th sample
+        """
+        X = []
+        y = []
+
+        for i in id_list:
+            X.append(self.getVector(i))
+            y.append(self.getLabel(i))
+
+        return X, y
