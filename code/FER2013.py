bf383605ea9438204c9bb06e3dbf7cc414b1b1d8
Tao Li <root@tao.li>
2020-10-25 17:22:56 -0400

    add sample code, add showImage(), update comment (use Honorio's style)

--- code/FER2013.py
+++ code/FER2013.py
@@ -2,6 +2,7 @@
 # A class to handle data reader, image encoding, etc.
 from tqdm import tqdm
 import numpy as np
+import cv2
 
 class FER2013:
 
@@ -12,6 +13,16 @@ class FER2013:
         self.X_dic = {}
         self.Y_dic = {}
 
+        self.label2expression = {
+            0: "Angry",
+            1: "Disgust",
+            2: "Fear",
+            3: "Happy",
+            4: "Sad",
+            5: "Surprise",
+            6: "Neutral"
+        }
+
         with open(filename) as f:
 
             print("Loading FER2013 dataset from {}".format(filename))
@@ -20,31 +31,38 @@ class FER2013:
             for line in tqdm(lines[1:]):
 
                 emotion, usage, pixels = line.split(",")
+                emotion = int(emotion)
                 pixels = np.asarray([int(j) for j in pixels.split(" ")])
 
-                self.X_dic[i] = self.encoding(pixels)
-                self.Y_dic[i] = emotion
+                img_id = "{:05d}".format(i)
+                self.X_dic[img_id] = pixels
+                self.Y_dic[img_id] = emotion
 
                 i += 1
 
-    def encoding(self, vec):
-        """
-        TODO modify this method in the future to explore different encoding methods.
-        Here we use one hot encoding first.
-        """
-        return vec
-
     def getVector(self, img_id):
         """
-        Returen feature vector
+        Input: image id
+        Output: vector vec of features, with 1 row, d columns (features)
+        TODO: Modify this method in the future to explore different encoding methods.
+              Here we use one hot encoding first.
         """
         return self.X_dic[img_id]
 
     def getLabel(self, img_id):
         """
-        Return image label
+        Input: img_id
+        Output: label
         """
         return self.Y_dic[img_id]
+
+    def getExpression(self, img_id):
+        """
+        Input: img_id
+        Output: expression
+        """
+        label = self.Y_dic[img_id]
+        return self.label2expression[label]
 
     def getTrainset(self, id_list):
         """
@@ -79,3 +97,22 @@ class FER2013:
             y.append(self.getLabel(i))
 
         return X, y
+
+    def showImage(self, img_id):
+        """
+        Input: image id
+        Output: none
+        Show image on the screen.
+        """
+        img = np.asarray(self.X_dic[img_id]).reshape((48, 48)) / 255
+        text = "image: {}, label: {} ({})".format(img_id, self.getLabel(img_id), self.getExpression(img_id))
+        cv2.imshow(text, img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+if __name__=="__main__":
+
+    # Example code
+    fer = FER2013("../data/sample.csv")
+    fer.showImage(img_id="00010")
+
