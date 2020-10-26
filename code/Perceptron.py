93fa0cd76e496400096bec04290a6466cc9b0a61
Tao Li <root@tao.li>
2020-10-25 22:22:59 -0400

    update perceptron algo (basically just a wrapper of sklearn.linear_model.Perceptron)

--- code/Perceptron.py
+++ code/Perceptron.py
@@ -1,6 +1,40 @@
 #!/usr/bin/env python3
-from sklearn.linear_model import Perceptron
+import sklearn.linear_model
 from FER2013 import FER2013
+
+class Perceptron(sklearn.linear_model.Perceptron):
+    """Perceptron
+
+    Reference: https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Perceptron.html
+    """
+
+    def train(self, X, y):
+        """
+        Input: matrix X of features, with n rows (samples), d columns (features)
+                   X(i,j) is the j-th feature of the i-th sample
+               vector y of labels, with n rows (samples), 1 column
+                   y(i) is the label (+1 or -1) of the i-th sample
+        """
+        self.fit(X, y)
+
+    def predict(self, X):
+        """
+        Input: matrix X of features, with n rows (samples), d columns (features)
+                   X(i,j) is the j-th feature of the i-th sample
+        Output: vector y of labels, with n rows (samples), 1 column
+                    y(i) is the label (+1 or -1) of the i-th sample
+        """
+        return super().predict(X)
+
+    def score(self, X, y):
+        """
+        Input: matrix X of features, with n rows (samples), d columns (features)
+                   X(i,j) is the j-th feature of the i-th sample
+               vector y of labels, with n rows (samples), 1 column
+                   y(i) is the label (+1 or -1) of the i-th sample
+        Output: scalar, mean accurary on the test set [X, y]
+        """
+        return super().score(X, y)
 
 if __name__=="__main__":
 
@@ -12,7 +46,7 @@ if __name__=="__main__":
     test_list = ["{:05d}".format(i) for i in range(20000, 25000)]
     X_test, y_test = fer.getSubset(test_list)
 
-    clf = Perceptron(tol=1e-3, random_state=0, verbose=1, n_jobs=8)
-    clf.fit(X_train, y_train)
-    print("mean accuracy:", clf.score(X_test, y_test))
+    model = Perceptron(tol=1e-3, random_state=0, verbose=1, n_jobs=8)
+    model.train(X_train, y_train)
+    print("mean accuracy:", model.score(X_test, y_test))
 
