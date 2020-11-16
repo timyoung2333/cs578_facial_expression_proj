ec18eb0b41c9d345b1dc44a23de73dd7dd51a978
Tao Li <root@tao.li>
2020-11-16 09:12:40 -0500

    ok version of simple CNN

--- code/CNN.py
+++ code/CNN.py
@@ -20,9 +20,9 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(16 * 9 * 9, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, 7)
 
         self.device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
         self.to(self.device)
@@ -30,19 +30,19 @@ class CNN(nn.Module):
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = x.view(-1, 16 * 9 * 9)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-    def train(self, X, y, epoch_num=100):
+    def train(self, X, y, epoch_num=1000):
 
         inputs = torch.Tensor(X.reshape((len(X), 1, 48, 48)))
         labels = torch.Tensor(y)
 
         inputs = inputs.to(self.device)
-        labels = labels.to(self.device)
+        labels = labels.to(self.device).long()
 
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.SGD(self.parameters(), lr=0.001, momentum=0.9)
@@ -59,7 +59,7 @@ class CNN(nn.Module):
             optimizer.step()
 
             # print loss
-            print("epoch: {}, loss: {:.3f}".format(epoch, loss.items()))
+            print("epoch: {}, loss: {:.3f}".format(epoch+1, loss.item()))
 
         print('Finished Training')
 
@@ -70,30 +70,16 @@ class CNN(nn.Module):
         torch.save(self.state_dict(), path)
         print("Model has been saved in {}".format(path))
 
+    def predict(self, X):
 
-    # def predict(self, testset):
+        inputs = torch.Tensor(X.reshape((len(X), 1, 48, 48)))
+        inputs = inputs.to(self.device)
+        outputs = self(inputs)
+        _, predicted = torch.max(outputs, 1)
 
-    #     testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-    #                                              shuffle=False, num_workers=2)
-    #     images, labels = data
-    #     outputs = self(testset)
-    #     _, predicted = torch.max(outputs.data, 1)
-    #     return predicted
+        return predicted.cpu().numpy()
 
-def main():
-
-    # transform = transforms.Compose(
-    #     [transforms.ToTensor(),
-    #      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-    # trainset = torchvision.datasets.CIFAR10(root='./tmp', train=True,
-    #                                         download=True, transform=transform)
-    # trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-    #                                           shuffle=True, num_workers=2)
-    # testset = torchvision.datasets.CIFAR10(root='./tmp', train=False,
-    #                                        download=True, transform=transform)
-    # testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-    #                                          shuffle=False, num_workers=2)
+if __name__ == "__main__":
 
     fer = FER2013("../data/sample.csv")
 
@@ -102,7 +88,5 @@ def main():
 
     cnn = CNN()
     cnn.train(X_train, y_train)
-    cnn.save()
-
-if __name__ == "__main__":
-    main()
+    y_hat = cnn.predict(X_train)
+    print("mean accuracy:", sum(y_train == y_hat) / len(y_train))
