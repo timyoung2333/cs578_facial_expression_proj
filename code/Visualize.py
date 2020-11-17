bbff18f591dcb58fe0f342b2f8842fc76896bff8
EdisonPrincehan <princehan920453024@gmail.com>
2020-11-17 16:40:42 +0800

    add method to load roc coordinates from multiple .csv files and plot them on one graph.

--- code/Visualize.py
+++ code/Visualize.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import label_binarize
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+import os
 
 label2expression = {
             0: "Angry",
@@ -211,7 +212,7 @@ class Visualize:
         plt.ylim([0.0, 1.05])
         plt.xlabel('False Positive Rate')
         plt.ylabel('True Positive Rate')
-        plt.title('Receiver Operating Characteristic of {}-fold Cross Validation of {}'.format(K, self.algo_name))
+        plt.title('Receiver Operating Characteristic (ROC) of {}-fold Cross Validation of {}'.format(K, self.algo_name))
         plt.legend(loc="lower right")
         plt.tight_layout()
         if save_fig_path != '':
@@ -219,8 +220,31 @@ class Visualize:
         else:
             plt.show()
 
-    # todo
-    # show ROC curves of multiples cross-validated models in one graph
+    def loadAndPlotRocCurve(self, roc_coords_paths=[]):
+        if roc_coords_paths:
+            plt.figure()
+            for path in roc_coords_paths:
+                # get algorithm name
+                pa, fi = os.path.split(path)
+                name, ext = os.path.splitext(fi)
+                # plot the coordinates
+                with open(path, 'r') as f:
+                    csv_reader = csv.reader(f)
+                    fpr = next(csv_reader)
+                    tpr = next(csv_reader)
+                    auc = self.aucByRate(fpr, tpr)
+                    plt.plot(fpr, tpr, lw=2, label='ROC curve of {0} (area = {1:0.2f})'.format(name, auc))
+            # plot random guess line for binary prediction
+            plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+            plt.xlim([0.0, 1.0])
+            plt.ylim([0.0, 1.05])
+            plt.xlabel('False Positive Rate')
+            plt.ylabel('True Positive Rate')
+            plt.title('Receiver Operating Characteristic (ROC) of Multiple Algorithms')
+            plt.legend(loc="lower right")
+            plt.tight_layout()
+        else:
+            print('Path list is empty!')
 
     def rocCurve(self, y_true, y_score):
         """
