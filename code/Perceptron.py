b62fc4d466885ecb03b86c99942ba39306eb6b83
Tao Li <root@tao.li>
2020-11-18 03:25:34 -0500

    run evaluation code in Perceptron (follow adaboost.py)

--- code/Perceptron.py
+++ code/Perceptron.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sklearn.linear_model
 from FER2013 import FER2013
+import numpy as np
 
 class Perceptron(sklearn.linear_model.Perceptron):
     """Perceptron
@@ -36,17 +37,47 @@ class Perceptron(sklearn.linear_model.Perceptron):
         """
         return super().score(X, y)
 
+    def set_params(self, params):
+        return super().set_params(**params)
+
 if __name__=="__main__":
 
-    fer = FER2013()
+    fer = FER2013(filename='../data/subset3500.csv')
+    from Evaluation import Evaluation
+    from Visualize import Visualize
 
-    train_list = ["{:05d}".format(i) for i in range(20000)]
-    X_train, y_train = fer.getSubset(train_list, encoding="raw_pixels+landmarks")
+    # Hyperparameter tuning
+    params = {
+        'penalty': ['l2', 'l1', 'elasticnet'],
+        'alpha': [0.0001, 0.001, 0.01],
+        'max_iter': [100, 500, 1000]
+    }
 
-    test_list = ["{:05d}".format(i) for i in range(20000, 25000)]
-    X_test, y_test = fer.getSubset(test_list, encoding="raw_pixels+landmarks")
+    # Subset size: 10 subset data sizes
+    samples_per_expression = np.arange(50, 500 + 1, 50)  # balanced sampling from all labels
 
-    model = Perceptron(tol=1e-3, random_state=0, verbose=1, n_jobs=8)
-    model.train(X_train, y_train)
-    print("mean accuracy:", model.score(X_test, y_test))
+    # Feature encoding
+    feature_encoding_methods = ['raw_pixels', 'raw_pixels+landmarks']
+
+    # Train models by cross validation and save results
+    k = 10  # k-fold Cross Validation
+    for encoding in feature_encoding_methods:
+        for subset_size in samples_per_expression:
+            eva = Evaluation(fer, subset_size, encoding)
+            model = Perceptron()
+            eva.gridSearchCV(k, model, param_grid=params,
+                             save_path='../result/Perceptron/{0}-subset{1}.csv'.format(encoding, subset_size))
+            print('Finished training for subset size {} of all parameters!'.format(subset_size))
+        print('Finished training for feature encoding {} of all subset sizes and parameters!'.format(encoding))
+    print('Finished all the training, congratulations!')
+
+    # train_list = ["{:05d}".format(i) for i in range(800)]
+    # X_train, y_train = fer.getSubset(train_list, encoding="raw_pixels+landmarks")
+
+    # test_list = ["{:05d}".format(i) for i in range(800, 1000)]
+    # X_test, y_test = fer.getSubset(test_list, encoding="raw_pixels+landmarks")
+
+    # model = Perceptron(tol=1e-3, random_state=0, verbose=1, n_jobs=8)
+    # model.train(X_train, y_train)
+    # print("mean accuracy:", model.score(X_test, y_test))
 
