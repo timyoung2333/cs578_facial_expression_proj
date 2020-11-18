26717fd93efd156cdffe8394faece6b61e157426
Tao Li <root@tao.li>
2020-11-18 12:17:57 -0500

    add SVM evaluation code

--- code/SVM.py
+++ code/SVM.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sklearn.svm
 from FER2013 import FER2013
+import numpy as np
 
 class SVM(sklearn.svm.SVC):
     """C-Support Vector Classification
@@ -36,17 +37,50 @@ class SVM(sklearn.svm.SVC):
         """
         return super().score(X, y)
 
+    def set_params(self, params):
+        return super().set_params(**params)
+
 if __name__=="__main__":
 
-    # Example code
-    fer = FER2013()
+    # # Example code
+    # fer = FER2013()
 
-    train_list = ["{:05d}".format(i) for i in range(20000)]
-    X_train, y_train = fer.getSubset(train_list)
+    # train_list = ["{:05d}".format(i) for i in range(20000)]
+    # X_train, y_train = fer.getSubset(train_list)
 
-    test_list = ["{:05d}".format(i) for i in range(20000, 25000)]
-    X_test, y_test = fer.getSubset(test_list)
+    # test_list = ["{:05d}".format(i) for i in range(20000, 25000)]
+    # X_test, y_test = fer.getSubset(test_list)
 
-    model = SVM(C=1.0, decision_function_shape='ovo', kernel='rbf', tol=0.001, verbose=True)
-    model.train(X_train, y_train)
-    print("mean accuracy:", model.score(X_test, y_test))
+    # model = SVM(C=1.0, decision_function_shape='ovo', kernel='rbf', tol=0.001, verbose=True)
+    # model.train(X_train, y_train)
+    # print("mean accuracy:", model.score(X_test, y_test))
+
+    fer = FER2013(filename='../data/subset3500.csv')
+    from Evaluation import Evaluation
+    from Visualize import Visualize
+
+    # Hyperparameter tuning
+    params = {
+        'C': [1.0, 2.0, 4.0, 8.0],
+        'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+        'gamma': ['scale', 'auto']
+    }
+
+    # Subset size: 10 subset data sizes
+    samples_per_expression = np.arange(50, 500 + 1, 50)  # balanced sampling from all labels
+
+    # Feature encoding
+    feature_encoding_methods = ['raw_pixels', 'raw_pixels+landmarks']
+
+    # Train models by cross validation and save results
+    k = 10  # k-fold Cross Validation
+    for encoding in feature_encoding_methods:
+        for subset_size in samples_per_expression:
+            eva = Evaluation(fer, subset_size, encoding)
+            model = SVM()
+            eva.gridSearchCV(k, model, param_grid=params,
+                             save_path='../result/SVM/{0}-subset{1}.csv'.format(encoding, subset_size))
+            print('Finished training for subset size {} of all parameters!'.format(subset_size))
+        print('Finished training for feature encoding {} of all subset sizes and parameters!'.format(encoding))
+    print('Finished all the training, congratulations!')
+
